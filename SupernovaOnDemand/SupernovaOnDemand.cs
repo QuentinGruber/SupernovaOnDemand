e617bc024378d86683a21f1012d1be5b3b5a6c34
Quentin Gruber <quentingruber@gmail.com>
2022-04-29 15:03:25 +0200

    init

---
+++ SupernovaOnDemand/SupernovaOnDemand.cs
@@ -0,0 +1,22 @@
+﻿using OWML.Common;
+using OWML.ModHelper;
+using UnityEngine.InputSystem;
+
+namespace SuperNovaOnDemand
+{
+    public class SuperNovaOnDemand : ModBehaviour
+    {
+        private void Start()
+        {
+            ModHelper.Console.WriteLine($"Mod {nameof(SuperNovaOnDemand)} is loaded!", MessageType.Success);
+        }
+
+        private void Update()
+        {
+            if (Keyboard.current[Key.N].wasPressedThisFrame)
+            {
+                GlobalMessenger.FireEvent("TriggerSupernova");
+            }
+        }
+    }
+}
